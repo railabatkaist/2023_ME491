72f3b29f604214da977d7aa53c66d18296b70467
Gwanghyeon Ji <36002072+karlji1021@users.noreply.github.com>
2023-10-18 17:49:47 +0900

    update getOptimalAction

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,10 @@
 #include "student_id.hpp"
 
 int main() {
-  Eigen::Vector<int, 12> state1, state2;
+  Eigen::Vector<int, 12> state;
 
-  std::cout << "optimal value for the state: " << getOptimalValue(state1) << std::endl;
-  std::cout << "optimal action sequence for the state: ";
-  printOptimalAction(state2);
+  std::cout << "optimal value for the state: " << getOptimalValue(state) << std::endl;
+  std::cout << "optimal action for the state: " << getOptimalAction(state) << std::endl;
 
   return 0;
 }
--- src/student_id.hpp
+++ src/student_id.hpp
@@ -4,14 +4,17 @@
 
 /// DO NOT CHANGE THE NAME AND FORMAT OF THIS FUNCTION
 double getOptimalValue(Eigen::Vector<int, 12> state){
-  // return the optimal value of the given state
+  // return the optimal value given the state
   /// TODO
-  return 0.0; // return optimal value
+
+  return 0.0;  // return optimal value
 }
 
 /// DO NOT CHANGE THE NAME AND FORMAT OF THIS FUNCTION
-void printOptimalAction(Eigen::Vector<int, 12> state){
-  // print the optimal action for the given state.
+int getOptimalAction(Eigen::Vector<int, 12> state){
+  // return one of the optimal actions given the state.
   // the action should be represented as a state index, at which a line will be drawn.
   /// TODO
+
+  return 0;  // return optimal action
 }
