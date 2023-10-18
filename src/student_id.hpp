20849b592c4dcfde3e8998aa21b9533ccd8c0d69
Jemin Hwangbo <jhwangbo@kaist.ac.kr>
2023-10-18 17:30:29 +0900

    Update student_id.hpp

--- src/student_id.hpp
+++ src/student_id.hpp
@@ -3,14 +3,14 @@
 #include <Eigen/Dense>
 
 /// DO NOT CHANGE THE NAME AND FORMAT OF THIS FUNCTION
-double getOptimalValue(Eigen::Vector<int, 12> state){
+double getOptimalValue(const Eigen::Vector<int, 12>& state){
   // return the optimal value of the given state
   /// TODO
   return 0.0; // return optimal value
 }
 
 /// DO NOT CHANGE THE NAME AND FORMAT OF THIS FUNCTION
-void printOptimalAction(Eigen::Vector<int, 12> state){
+void printOptimalAction(const Eigen::Vector<int, 12>& state){
   // print the optimal action for the given state.
   // the action should be represented as a state index, at which a line will be drawn.
   /// TODO
